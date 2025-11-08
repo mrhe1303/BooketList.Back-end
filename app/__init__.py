7d1254375353b43a26fab0b6add773f8b4baa464
mjfy93 <maria.jose.ferro@hotmail.com>
2025-11-07 19:07:30 -0500

    origins changed

--- app/__init__.py
+++ app/__init__.py
@@ -24,7 +24,9 @@ def create_app():
                         "http://localhost:5173", 
                         "http://127.0.0.1:5173",
                         "http://localhost:3000",
-                        "http://127.0.0.1:3000"
+                        "http://127.0.0.1:3000",
+                        "https://front-end-seven-swart.vercel.app"
+                        
                     ],
                     "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
                     "allow_headers": ["Content-Type", "Authorization", "Access-Control-Allow-Origin"],
